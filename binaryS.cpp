be1ab0b7e26066e10ce19b9db319d1e9babcdd68
harmanSandhu-glitch <harmanjot0596.be23@chitkara.edu.in>
2024-02-28 13:19:46 +0530

    first commit

---
+++ binaryS.cpp
@@ -0,0 +1,18 @@
+// #include <iostream>
+// using namespace std;
+// void binarySearch(int arr[], int start, int end, int numberToSearch)
+// {
+//     int mid = (end + start) / 2;
+//     if( numberToSearch > arr[mid]){
+//         binarySearch(arr, mid+1, end, numberToSearch);
+//     }
+//     else if (numberToSearch < arr[mid]){
+//         binarySearch(arr, start,mid,numberToSearch);
+//     };
+
+// }
+// int main(){
+//     int arr[] = {1,2,3,4,5,6,7};
+
+
+// };
